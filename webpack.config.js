cda5eb4ba6a4dd13196b673ccd181f2c33819e38
Victor Alves <victor.alves@leadforce.com.br>
2022-12-15 22:59:50 -0300

    update project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
     },
-    mode: 'development',
-    module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }]
-    }
+    {
+      use: ["style-loader", "css-loader", "sass-loader"],
+      test: /.(css|sass|scss)$/,
+    },
+    {
+      use: ["html-loader"],
+      test: /\.html$/i,
+    },
+  ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 5050,
+    open: true,
+  },
 };
