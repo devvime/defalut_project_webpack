1a364da6d16bf38310eacc9b1bb94fef0ad012d4
Victor Alves <victor.alves@leadforce.com.br>
2022-12-23 02:15:27 -0300

    fix structure

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,9 @@
 import { blots } from 'blots'
+import Mustache from 'mustache'
 import $ from 'jquery'
 import './home.sass'
 import html from './home.html'
+import drop from './drops.html'
 
 export default class Home {
 
@@ -15,37 +17,26 @@ export default class Home {
   }
 
   rain() {
-    var makeItRain = function() {
-      $('.rain').empty()    
-      var increment = 0
-      var drops = ""
-      var backDrops = ""    
-      while (increment < 100) {
-        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1))
-        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2))
-        increment += randoFiver
-        drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>'
-        backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>'
-      }    
-      $('.rain.front-row').append(drops)
-      $('.rain.back-row').append(backDrops)
-    }    
-    $('.splat-toggle.toggle').on('click', function() {
-      $('body').toggleClass('splat-toggle')
-      $('.splat-toggle.toggle').toggleClass('active')
-      makeItRain()
-    })    
-    $('.back-row-toggle.toggle').on('click', function() {
-      $('body').toggleClass('back-row-toggle')
-      $('.back-row-toggle.toggle').toggleClass('active')
-      makeItRain()
-    })    
-    $('.single-toggle.toggle').on('click', function() {
-      $('body').toggleClass('single-toggle')
-      $('.single-toggle.toggle').toggleClass('active')
-      makeItRain()
-    })
-    makeItRain()
+    $('.rain').empty()    
+    let increment = 0
+    let drops = ""
+    let backDrops = ""    
+    while (increment < 100) {
+      let randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1))
+      let randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2))
+      increment += randoFiver
+
+      const values = {
+        increment,
+        randoFiver: (randoFiver + randoFiver - 1 + 100),
+        randoHundo
+      }
+
+      drops += Mustache.render(drop, {...values, direction: 'left'})
+      backDrops += Mustache.render(drop, {...values, direction: 'right'})
+    }        
+    $('.rain.front-row').append(drops)
+    $('.rain.back-row').append(backDrops)
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import '../node_modules/bootstrap/dist/js/bootstrap'
 import './components/default/theme/theme.sass'
 import { blots } from 'blots'
-import Nav from './components/nav/nav.js'
 import Home from './components/home/home.js'
-import listCars from './components/listCars/listCars.js'
 import pageNotFoundComponent from './components/default/pageNotFound/pageNotFoundComponent.js'
 
-blots.render(Nav)
-
 blots.route('/', Home)
-blots.route('/cars', listCars)
 blots.route('*', pageNotFoundComponent)
 
-blots.run()
+blots.start()
