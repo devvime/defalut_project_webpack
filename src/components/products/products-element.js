27107db408b3ffb07fd00b8bf2129f28c5cd767e
devvime <dihapp@outlook.com>
2024-05-21 18:25:59 -0300

    update blots version

--- src/components/products/products-element.js
+++ src/components/products/products-element.js
@@ -8,6 +8,7 @@ export const Products = {
   strict: {
     products: [
       {
+        id: new Date().getTime(),
         title: 'Notebook',
         price: 3599.99
       }
@@ -19,16 +20,25 @@ export const Products = {
   },
   actions() {
     click('add-product', Products.addProduct)
+    click('remove-product', Products.removeProduct)
   },
   addProduct() {
     let form = document.querySelector('#add-product')
-    if (form.title.value === '') return
-    if (form.price.value === '') return
+    if (form.title.value === '' || form.price.value === '') return
     Products.strict.products.push({
+      id: new Date().getTime(),
       title: form.title.value,
       price: form.price.value,
     })
     strict.save()
+  },
+  removeProduct() {
+    const id = Number(this.getAttribute('id'))
+    Products.strict.products = Products.strict.products.filter(p => p.id !== id)
+    strict.save()
+  },
+  test() {
+    console.log(Products.strict.products)
   }
 }
 
